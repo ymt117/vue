aa9bd32c0727ddbafb6d0b685110fa15f5311a8b
k3402312 <k3402312@kadai.jp>
2018-07-07 16:23:46 +0900

    切り替え

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var app = new Vue({
     el: "#app",
     data: {
         message: "Hello Vue.js!",
-        list: ["apple", "banana", "strawberry"]
+        list: ["apple", "banana", "strawberry"],
+        count: "pls Input number",
+        show: true
     },
     methods: {
         handleClick: function(event){
