5ff8c6f476ff829080f764e4254187a9db525588
Yamato <k3402312@kadai.jp>
2018-07-21 20:34:32 +0900

    モンスターの追加と削除

--- main.js
+++ main.js
@@ -4,11 +4,30 @@ var app = new Vue({
         message: "Hello Vue.js!",
         list: ["apple", "banana", "strawberry"],
         count: "pls Input number",
-        show: true
+        show: true,
+        name: '',
+        monster: [
+            {id: 1, name: 'スライム', hp: 100},
+            {id: 2, name: 'ゴブリン', hp: 200},
+            {id: 3, name: 'ドラゴン', hp: 500}
+        ]
     },
     methods: {
         handleClick: function(event){
             alert(event.target)
+        },
+        doAdd: function(){
+            var max = this.monster.reduce(function(a, b){
+                return a.id > b.id ? a.id : b.id
+            }, 0)
+            this.monster.push({
+                id: max + 1,
+                name: this.name,
+                hp: 500
+            })
+        },
+        doRemove: function(index){
+            this.monster.splice(index, 1)
         }
     }
 })
